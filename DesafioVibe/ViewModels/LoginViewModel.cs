77ffa414a07c5d6dd37eb00d1a91e06967c15819
Victor Hugo Telles <tellesvictorhugo@gmail.com>
2019-11-23 14:28:46 -0300

    * Padrão MVVM aplicado na tela de login. * Ajustes na interface;

---
+++ DesafioVibe/ViewModels/LoginViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using DesafioVibe.Views;
+
+namespace DesafioVibe.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LoginViewModel()
+        {
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+
+        public Command LoginCommand
+        {
+            get
+            {
+                return new Command(Login);
+            }
+        }
+
+        private void Login()
+        {
+            if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))
+                Application.Current.MainPage.DisplayAlert("Valores Vazios", "Por favor, verifique se todos os campos estão preenchidos.", "OK");
+            else
+            {
+                // TODO Fazer requisição
+                if (Email == "abc@gmail.com" && Password == "1234")
+                {
+                    // Application.Current.MainPage.DisplayAlert("Login bem sucedido!", "", "OK");
+                    Application.Current.MainPage.Navigation.PushAsync(new WelcomePage());
+                }
+                else
+                    Application.Current.MainPage.DisplayAlert("Falha no login.", "Por favor, entre tente novamente.", "OK");
+            }
+        }
+    }
+}
--- DesafioVibe/Views/LoginPage.xaml.cs
+++ DesafioVibe/Views/LoginPage.xaml.cs
@@ -1,33 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using DesafioVibe.ViewModels;
 using Xamarin.Forms;
 
 namespace DesafioVibe.Views
 {
     public partial class LoginPage : ContentPage
     {
+        LoginViewModel loginViewModel;
         public LoginPage()
         {
+            loginViewModel = new LoginViewModel();
             InitializeComponent();
-        }
-
-        private void btnLogin_Clicked(object sender, EventArgs e)
-        {
-            if (string.IsNullOrEmpty(Email.Text) || string.IsNullOrEmpty(Password.Text))
-            {
-                DisplayAlert("Valores Vazios", "Por favor, verifique se todos os campos estão preenchidos.", "OK");
-            } else
-            {
-                // Fazer requisição
-                if (Email.Text == "abc@gmail.com" && Password.Text == "1234")
-                {
-                    DisplayAlert("Login bem sucedido!", "", "OK");
-                    Navigation.PushAsync(new WelcomePage());
-                }
-                else
-                    DisplayAlert("Falha no login.", "Por favor, entre tente novamente.", "OK");
-            }
+            BindingContext = loginViewModel;
         }
     }
 }
