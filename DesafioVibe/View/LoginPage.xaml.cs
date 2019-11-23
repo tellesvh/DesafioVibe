a44de4b0657a231cf39191657f8c9f869bf1064f
Victor Hugo Telles <tellesvictorhugo@gmail.com>
2019-11-23 13:48:48 -0300

    Commit inicial

---
+++ DesafioVibe/View/LoginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DesafioVibe.View
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Email.Text) || string.IsNullOrEmpty(Password.Text))
+            {
+                DisplayAlert("Valores Vazios", "Por favor, verifique se todos os campos estão preenchidos.", "OK");
+            } else
+            {
+                // Fazer requisição
+                if (Email.Text == "abc@gmail.com" && Password.Text == "1234")
+                {
+                    DisplayAlert("Login bem sucedido!", "", "OK");
+                    Navigation.PushAsync(new WelcomePage());
+                }
+                else
+                    DisplayAlert("Falha no login.", "Por favor, entre tente novamente.", "OK");
+            }
+        }
+    }
+}
---
+++ DesafioVibe/View/WelcomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DesafioVibe.View
+{
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
